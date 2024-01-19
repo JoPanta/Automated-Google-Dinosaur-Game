d7e1b3a3edf87f5bb2ecb8d0ac708e635aa0eae1
João Pantaleão <jopantaleao@gmail.com>
2024-01-19 23:22:19 +0000

    done

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from PIL import Image
-from io import BytesIO
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
+from selenium.webdriver.common.keys import Keys
+from PIL import ImageGrab, ImageOps
+from time import sleep
+
 
 # to keep browser open
 chrome_options = webdriver.ChromeOptions()
@@ -18,54 +15,38 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.maximize_window()
 driver.get('https://chromedino.com/')
 
+dino = (649, 303, 700, 350)
+box = (780, 298, 820, 345)
 
-# wait for elements to appear, if necessary
-cookies = (WebDriverWait(driver, 10).until
-           (EC.element_to_be_clickable((By.XPATH, '//*[@id="t"]/div[3]/div[2]/div[1]/div[2]/div[2]/button[1]'))))
+cookies = driver.find_element(By.XPATH, value="/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/button[1]")
 cookies.click()
 
-canvas = driver.find_element(By.XPATH, value='//*[@id="main-frame-error"]/div[2]/canvas')
-print(canvas.location, canvas.size)
 
-# pressing space
+def take_print():
+    sleep(0.02)
+    bbox = (box[0], box[1], box[2], box[3])
+    image = ImageGrab.grab(bbox)
+    gray = ImageOps.grayscale(image)
+    image.save("box.png")
+    color = gray.getcolors()
+    total = sum(map(sum, gray.getcolors()))
+    return total
 
-driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.SPACE)
+print(take_print())
 
-
-def is_color_present(image, target_color):
-    # open image
-
-    im = Image.open(BytesIO(image))
-
-    # crop image
-    cropped_img = im.crop((840, 180, 870, 220))
-
-    # iterate through each pixel
-
-    for x in range(cropped_img.width):
-        for y in range(cropped_img.height):
-            # Get the RGB values of the current pixel
-            pixel_color = cropped_img.getpixel((x, y))
-
-            if pixel_color == target_color:
-                return True
-
-    # If the target color is not found in the cropped image
-    return False
+def jump():
+    driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.SPACE)
 
 
-target_color = (83, 83, 83, 255)
+def main():
+    jump()
+    while True:
+        take_print()
+        if take_print() < 2100 or take_print() > 2200:
+            jump()
 
-image = driver.get_screenshot_as_png()
 
+if __name__ == "__main__":
+    main()
 
-
-while True:
-    # saves screenshot of entire page
-    image = driver.get_screenshot_as_png()
-
-    result = is_color_present(image, target_color)
-    if result:
-        driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.SPACE)
-    else:
-        pass
+print(take_print())
